31b9d6f04ee0c454b64711a28770f012c8012511
Jan Madeyski <jan.madeyski@gmail.com>
2025-04-20 19:04:39 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/status', fn () => ['status' => 'ok'])
-    ->withoutMiddleware(['api']);
-
 Route::prefix('v1')->group(function () {
     Route::apiResource('tenants', TenantController::class);
 
