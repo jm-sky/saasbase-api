b7f8b199fff8ce8df03657b01007aaf2ae7da02c
Jan Madeyski <jan.madeyski@gmail.com>
2025-04-20 19:07:34 +0200

    Create AuthController.php

---
+++ app/Domain/Auth/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+namespace App\Domain\Auth\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth()->factory()->getTTL() * 60,
+            'user'         => auth()->user(),
+        ]);
+    }
+}
