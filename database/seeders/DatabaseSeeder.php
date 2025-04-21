b5dafb08e87145714b2607020ecb19e6d3415074
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-21 13:54:00 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Domain\Auth\Models\User;
 use App\Domain\Tenant\Models\Tenant;
+use App\Domain\Contractor\Models\Contractor;
+use App\Domain\Product\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -36,6 +38,14 @@ class DatabaseSeeder extends Seeder
             SkillSeeder::class,
             ProjectRoleSeeder::class,
             MeasurementUnitSeeder::class,
+        ]);
+
+        Contractor::factory(5)->create([
+            'tenant_id' => \App\Domain\Tenant\Models\Tenant::first()->id,
+        ]);
+        
+        Product::factory(5)->create([
+            'tenant_id' => \App\Domain\Tenant\Models\Tenant::first()->id,
         ]);
     }
 }
