f3792ff646615f8b0aeb6fbd1c21b376b169d8a9
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-21 23:40:26 +0200

    Update 2024_03_19_000000_create_contractors_table.php

--- database/migrations/2024_03_19_000000_create_contractors_table.php
+++ database/migrations/2024_03_19_000000_create_contractors_table.php
@@ -11,7 +11,7 @@ return new class() extends Migration {
             $table->id();
             $table->uuid('tenant_id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
