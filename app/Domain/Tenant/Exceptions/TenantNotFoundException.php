7cf77bdb5c8a295f8ab68bed9d30ab9b9f41df28
Jan Madeyski <jan.madeyski@gmail.com>
2025-04-22 13:15:32 +0200

    Tenant context from JWT and scope

---
+++ app/Domain/Tenant/Exceptions/TenantNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Domain\Tenant\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Response as ResponseFacade;
+
+class TenantNotFoundException extends Exception
+{
+    protected $message = 'Tenant context not found. Please ensure the user is properly authenticated for the correct tenant.';
+
+    public function render($request): JsonResponse
+    {
+        return ResponseFacade::json([
+            'error' => $this->message,
+        ], Response::HTTP_FORBIDDEN);
+    }
+} 
