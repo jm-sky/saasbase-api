6cc261d06fd85e6c337441d3d0e879dcf440cdbc
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-24 10:44:11 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->register(\App\Domain\Tenant\TenantEventServiceProvider::class);
     }
 
     /**
