682f5461ff4c3a7c44afc17682b2366683f75ab3
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-24 14:12:57 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,23 +24,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/v1/auth/login', [AuthController::class, 'login']);
-
-Route::post('/v1/auth/logout', [AuthController::class, 'logout']);
-
-Route::post('/v1/auth/register', [AuthController::class, 'register']);
-
-Route::middleware('auth:api')->post('/v1/auth/refresh', [AuthController::class, 'refresh']);
-
-Route::prefix('v1/oauth')->group(function () {
-    Route::get('{provider}/redirect', [OAuthController::class, 'redirect']);
-    Route::get('{provider}/callback', [OAuthController::class, 'callback']);
-});
-
 Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('register', [AuthController::class, 'register']);
+    });
+    
+    Route::prefix('oauth')->group(function () {
+        Route::get('{provider}/redirect', [OAuthController::class, 'redirect']);
+        Route::get('{provider}/callback', [OAuthController::class, 'callback']);
+    });
+    
     Route::apiResource('tenants', TenantController::class);
 
     Route::middleware('auth:api')->group(function () {
+        Route::post('auth/refresh', [AuthController::class, 'refresh']);
         Route::get('user', [AuthController::class, 'getUser']);
         Route::apiResource('contractors', ContractorController::class);
         Route::apiResource('products', ProductController::class);
