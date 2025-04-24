7433361604c05879100889b210434f514cc465e0
Jan Madeyski <jan.madeyski@gmail.com>
2025-04-24 19:23:43 +0200

    s3 config

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,14 +47,13 @@ return [
 
         's3' => [
             'driver'                  => 's3',
-            'key'                     => env('S3_ACCESS_KEY_ID'),
-            'secret'                  => env('S3_SECRET_ACCESS_KEY'),
-            'region'                  => env('S3_DEFAULT_REGION'),
+            'key'                     => env('S3_ACCESS_KEY'),
+            'secret'                  => env('S3_SECRET_KEY'),
+            'region'                  => env('S3_REGION'),
             'bucket'                  => env('S3_BUCKET'),
-            'url'                     => env('S3_URL'),
             'endpoint'                => env('S3_ENDPOINT'),
             'use_path_style_endpoint' => env('S3_USE_PATH_STYLE_ENDPOINT', false),
-            'throw'                   => false,
+            'throw'                   => true, //false,
             'report'                  => false,
         ],
     ],
