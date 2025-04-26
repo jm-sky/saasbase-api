8937c9f1a95257a99f21a46d21bf1b485f626eab
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-26 22:33:26 +0200

    Create CompanyLookupResultDTO.php

---
+++ app/Domain/CompanyLookup/DTOs/CompanyLookupResultDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\CompanyLookup\DTOs;
+
+class CompanyLookupResultDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $nip,
+        public readonly ?string $regon,
+        public readonly ?string $address,
+        public readonly ?string $vatStatus,
+    ) {}
+
+    public static function fromApiResponse(array $data): self
+    {
+        return new self(
+            name: $data['name'] ?? '',
+            nip: $data['nip'] ?? '',
+            regon: $data['regon'] ?? null,
+            address: $data['workingAddress'] ?? null,
+            vatStatus: $data['statusVat'] ?? null,
+        );
+    }
+}
