3a74aec924d1340d4c0901f1eb24b5d0121c269d
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-26 21:46:52 +0200

    Create user.php in routes/api

---
+++ routes/api/user.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Domain\Auth\Controllers\AuthController;
+use App\Domain\Auth\Controllers\UserSettingsController;
+use App\Domain\Auth\Controllers\UserProfileImageController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->prefix('user')->group(function () {
+    Route::get('profile', [AuthController::class, 'getUser']); // TODO: Move to UserController or sth.
+    Route::get('settings', [UserSettingsController::class, 'show']);
+    Route::put('settings', [UserSettingsController::class, 'update']);
+    Route::patch('settings/language', [UserSettingsController::class, 'updateLanguage']);
+    Route::post('profile-image', [UserProfileImageController::class, 'upload']);
+    Route::get('profile-image', [UserProfileImageController::class, 'show']);
+    Route::delete('profile-image', [UserProfileImageController::class, 'delete']);
+});
