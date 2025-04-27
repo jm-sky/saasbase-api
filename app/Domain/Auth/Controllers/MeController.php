d4d5b79aa1aedd1101f3b9ee5ede90cf3c24a607
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-27 23:59:05 +0200

    Update MeController.php

--- app/Domain/Auth/Controllers/MeController.php
+++ app/Domain/Auth/Controllers/MeController.php
@@ -3,12 +3,24 @@
 namespace App\Domain\Auth\Controllers;
 
 use Illuminate\Http\Request;
+use App\Domain\Auth\DTOs\UserDTO;
 use App\Http\Controllers\Controller;
 
 class MeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return response()->json($request->user());
+        $user = auth()->user();
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        // Transform the user model into a UserDTO
+        $userDTO = UserDTO::fromModel($user);
+
+        return response()->json([
+            'user' => $userDTO,
+        ]);
     }
 }
