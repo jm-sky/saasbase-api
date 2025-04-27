8e29615cdbb39092f919a09cab5a6f9a949de4bb
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-27 08:06:26 +0200

    Create ViesLookupResultDTO.php

---
+++ app/Domain/ViesLookup/DTOs/ViesLookupResultDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\ViesLookup\DTOs;
+
+class ViesLookupResultDTO
+{
+    public function __construct(
+        public readonly string $countryCode,
+        public readonly string $vatNumber,
+        public readonly bool $valid,
+        public readonly ?string $name,
+        public readonly ?string $address,
+    ) {}
+
+    public static function fromApiResponse(array $data): self
+    {
+        return new self(
+            countryCode: $data['countryCode'] ?? '',
+            vatNumber: $data['vatNumber'] ?? '',
+            valid: (bool) ($data['valid'] ?? false),
+            name: $data['name'] ?? null,
+            address: $data['address'] ?? null,
+        );
+    }
+}
