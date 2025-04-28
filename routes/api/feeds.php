f1af889518d597d04556039250f2db85b1d47b5e
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-28 12:53:09 +0200

    Create feeds.php

---
+++ routes/api/feeds.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Domain\Feeds\Controllers\FeedController;
+use App\Domain\Feeds\Controllers\FeedCommentController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/feeds', [FeedController::class, 'index']);
+    Route::post('/feeds', [FeedController::class, 'store']);
+    Route::get('/feeds/{feed}', [FeedController::class, 'show']);
+    Route::delete('/feeds/{feed}', [FeedController::class, 'destroy']);
+
+    Route::post('/feeds/{feed}/comments', [FeedCommentController::class, 'store']);
+    Route::delete('/feed-comments/{comment}', [FeedCommentController::class, 'destroy']);
+});
