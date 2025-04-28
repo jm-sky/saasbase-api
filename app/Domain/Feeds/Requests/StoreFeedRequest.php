be6f46534567583969268de768c72492380e97c7
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-28 13:07:02 +0200

    Create StoreFeedRequest.php

---
+++ app/Domain/Feeds/Requests/StoreFeedRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Domain\Feeds\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFeedRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true; // lub dodaj własną logikę autoryzacji jeśli potrzeba
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required', 'string'],
+        ];
+    }
+}
