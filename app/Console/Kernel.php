216737a582a05f88ded5b7e4eaeb5192f0266bb5
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-28 22:39:43 +0200

    Create Kernel.php

---
+++ app/Console/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    protected function commands(): void
+    {
+        $this->load(base_path('app/Domain/CompanyLookup/Commands'));
+        // Load other domain commands if needed
+        require base_path('routes/console.php');
+     }
+}
