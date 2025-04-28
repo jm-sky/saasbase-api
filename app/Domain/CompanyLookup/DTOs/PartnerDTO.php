01f1bf9571a1e08d6761ca784092e2a9d58ca4ee
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-04-28 12:09:55 +0200

    Create PartnerDTO.php

---
+++ app/Domain/CompanyLookup/DTOs/PartnerDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\CompanyLookup\DTOs;
+
+/**
+ * Partner Data Transfer Object
+ */
+class PartnerDTO
+{
+    public function __construct(
+        public readonly ?string $name,
+        public readonly ?string $nip,
+        public readonly ?string $pesel,
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            name: $data['name'] ?? null,
+            nip: $data['nip'] ?? null,
+            pesel: $data['pesel'] ?? null,
+        );
+    }
+}
