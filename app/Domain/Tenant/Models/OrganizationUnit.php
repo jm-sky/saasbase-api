8b92d63d744081f873caa5f989f1e9af2bad1b55
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-05-02 12:07:34 +0200

    Create OrganizationUnit.php

---
+++ app/Domain/Tenant/Models/OrganizationUnit.php
@@ -0,0 +1,55 @@
+<?php
+// app/Domain/Tenant/Models/OrganizationUnit.php
+
+namespace App\Domain\Tenant\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Tenant;
+
+class OrganizationUnit extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['id', 'tenant_id', 'parent_id', 'name', 'short_name'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id ??= (string) Str::uuid();
+        });
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    public function orgUnitUsers()
+    {
+        return $this->hasMany(OrgUnitUser::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'org_unit_user')
+            ->withPivot('role')
+            ->withTimestamps ();
+    }
+}
