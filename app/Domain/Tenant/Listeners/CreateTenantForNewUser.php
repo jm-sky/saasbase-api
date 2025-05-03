148c6e68fc23a71e4e70ed1c3677f3a021bca913
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-05-03 10:37:41 +0200

    Update CreateTenantForNewUser.php

--- app/Domain/Tenant/Listeners/CreateTenantForNewUser.php
+++ app/Domain/Tenant/Listeners/CreateTenantForNewUser.php
@@ -4,6 +4,7 @@ namespace App\Domain\Tenant\Listeners;
 
 use App\Domain\Auth\Events\UserCreated;
 use App\Domain\Tenant\Models\Tenant;
+use Illuminate\Support\Str;
 
 class CreateTenantForNewUser
 {
@@ -11,6 +12,7 @@ class CreateTenantForNewUser
     {
         $tenant = Tenant::create([
             'name' => "{$event->user->first_name}'s workspace",
+            'slug' => Str::slug(Str::before($event->user->email, '@')),
         ]);
 
         $event->user->tenants()->attach($tenant, ['role' => 'admin']);
