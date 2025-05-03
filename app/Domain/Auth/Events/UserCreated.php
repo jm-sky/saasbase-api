9f672d01f222134160a1d996c3993128e28d3d3c
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-05-03 08:46:26 +0200

    Create UserCreated.php

---
+++ app/Domain/Auth/Events/UserCreated.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Auth\Events;
+
+use App\Domain\Auth\Models\User;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserCreated
+{
+    use Dispatchable, SerializesModels;
+
+    public User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+}
