ab5559e19bc6299071e22807bfe958a54754bb21
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-05-16 21:39:21 +0200

    Update tenants.php

--- routes/api/tenants.php
+++ routes/api/tenants.php
@@ -25,12 +25,12 @@ Route::middleware(['auth:api', 'is_active', 'is_in_tenant'])->group(function () 
         })
     ;
 
-    Route::apiResource('tenants/{tenant}/addresses', TenantAddressController::class)->name('tenants.addresses');
+    Route::apiResource('tenants/{tenant}/addresses', TenantAddressController::class)->names('tenants.addresses');
     Route::post('tenants/{tenant}/addresses/{address}/set-default', [TenantAddressController::class, 'setDefault'])
         ->name('tenants.addresses.setDefault')
     ;
 
-    Route::apiResource('tenants/{tenant}/bank-accounts', TenantBankAccountController::class)->name('tenants.bankAccounts');
+    Route::apiResource('tenants/{tenant}/bank-accounts', TenantBankAccountController::class)->names('tenants.bankAccounts');
     Route::post('tenants/{tenant}/bank-accounts/{bankAccount}/set-default', [TenantBankAccountController::class, 'setDefault'])
         ->name('tenants.bankAccounts.setDefault')
     ;
