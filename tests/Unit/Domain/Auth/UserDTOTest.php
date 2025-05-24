65d6b6275ace6398b91487d699b864801b8787ec
Jan Madeyski <jan.madeyski@gmail.com>
2025-05-24 02:37:09 +0200

    fix tests

--- tests/Unit/Domain/Auth/UserDTOTest.php
+++ tests/Unit/Domain/Auth/UserDTOTest.php
@@ -23,8 +23,6 @@ class UserDTOTest extends TestCase
             'first_name'  => 'John',
             'last_name'   => 'Doe',
             'email'       => 'test@example.com',
-            'description' => 'Test description',
-            'birth_date'  => '1990-01-01',
             'phone'       => '+1234567890',
             'is_admin'    => true,
         ]);
@@ -35,8 +33,6 @@ class UserDTOTest extends TestCase
         $this->assertEquals($user->first_name, $dto->firstName);
         $this->assertEquals($user->last_name, $dto->lastName);
         $this->assertEquals($user->email, $dto->email);
-        $this->assertEquals($user->description, $dto->description);
-        $this->assertEquals($user->birth_date, $dto->birthDate);
         $this->assertEquals($user->phone, $dto->phone);
         $this->assertEquals($user->is_admin, $dto->isAdmin);
         $this->assertEquals($user->created_at?->toIso8601String(), $dto->createdAt->toIso8601String());
@@ -54,8 +50,6 @@ class UserDTOTest extends TestCase
             lastName: 'Doe',
             email: 'test@example.com',
             id: '123e4567-e89b-12d3-a456-426614174000',
-            description: 'Test description',
-            birthDate: '1990-01-01',
             phone: '+1234567890',
             isAdmin: true,
             createdAt: $now,
@@ -69,8 +63,6 @@ class UserDTOTest extends TestCase
         $this->assertEquals('John', $array['firstName']);
         $this->assertEquals('Doe', $array['lastName']);
         $this->assertEquals('test@example.com', $array['email']);
-        $this->assertEquals('Test description', $array['description']);
-        $this->assertEquals('1990-01-01', $array['birthDate']);
         $this->assertEquals('+1234567890', $array['phone']);
         $this->assertTrue($array['isAdmin']);
         $this->assertEquals($isoNow, $array['createdAt']);
