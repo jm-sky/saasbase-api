4891881bd06b3a780eb4604996b850aa381714e2
Jan Madeyski <jan.madeyski@gmail.com>
2025-05-27 23:35:13 +0200

    Create GusReportResolver.php

---
+++ app/Services/GusLookup/GusReportResolver.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services\GusLookup;
+
+use App\Services\GusLookup\Enums\EntityType;
+use App\Services\GusLookup\Enums\GusReportName;
+
+class GusReportResolver
+{
+    public static function resolve(array $basicRecord): ?GusReportName
+    {
+        $type = EntityType::tryFrom($basicRecord['Typ'] ?? '');
+
+        return match ($type) {
+            EntityType::P  => GusReportName::BIR11OsPrawna,
+            EntityType::LP => GusReportName::BIR11JednLokalnaOsPrawnej,
+            EntityType::LF => GusReportName::BIR11JednLokalnaOsFizycznej,
+
+            EntityType::F  => self::resolveForFizyczna($basicRecord),
+
+            default => null,
+        };
+    }
+
+    protected static function resolveForFizyczna(array $record): GusReportName
+    {
+        $silosId = $record['SilosID'] ?? null;
+
+        return match ($silosId) {
+            '6' => GusReportName::BIR11OsFizycznaRolnicza,
+            '4' => GusReportName::BIR11OsFizycznaDzialalnoscCeidg,
+            default => GusReportName::BIR11OsFizycznaDzialalnoscPozostala,
+        };
+    }
+}
