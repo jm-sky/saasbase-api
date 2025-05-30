b2810913a429069ee4939cb6e4e6563360676cf1
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-05-30 20:21:55 +0200

    Update skills.php

--- routes/api/skills.php
+++ routes/api/skills.php
@@ -2,11 +2,9 @@
 
 use App\Domain\Skills\Controllers\SkillCategoryController;
 use App\Domain\Skills\Controllers\SkillController;
-use App\Domain\Skills\Controllers\UserSkillController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api', 'is_active'])->group(function () {
     Route::apiResource('skills', SkillController::class);
     Route::apiResource('skill-categories', SkillCategoryController::class);
-    Route::apiResource('user-skills', UserSkillController::class);
 });
