1432352f948e682c4b57a7264f3cbcf68e82b332
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-06-03 11:39:38 +0200

    Update PlanSeeder.php

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -11,33 +11,42 @@ class PlanSeeder extends Seeder
     {
         $plans = [
             [
+                'name'              => 'Free',
+                'description'       => 'Free plan for discovering app features',
+                'stripe_product_id' => null,
+                'stripe_price_id'   => null,
+                'interval'          => 'monthly',
+                'price'             => 0.00,
+            ],
+            [
                 'name'              => 'Basic',
                 'description'       => 'Basic plan for small businesses',
-                'stripe_product_id' => 'prod_basic',
-                'stripe_price_id'   => 'price_basic_monthly',
+                'stripe_product_id' => 'prod_SOGm0E5JZd3VmE',
+                'stripe_price_id'   => 'price_1RTUEXK5z7ZAQPXQ076ndzjF',
                 'interval'          => 'monthly',
-                'price'             => 49.99,
+                'price'             => 50.00,
             ],
             [
                 'name'              => 'Pro',
                 'description'       => 'Professional plan for growing businesses',
-                'stripe_product_id' => 'prod_pro',
-                'stripe_price_id'   => 'price_pro_monthly',
+                'stripe_product_id' => 'prod_SOGnMpksxZBBrc',
+                'stripe_price_id'   => 'price_1RTUFRK5z7ZAQPXQmd5XG0lj',
                 'interval'          => 'monthly',
-                'price'             => 99.99,
+                'price'             => 100.00,
             ],
             [
                 'name'              => 'Full',
                 'description'       => 'Full access plan with all features',
-                'stripe_product_id' => 'prod_full',
-                'stripe_price_id'   => 'price_full_monthly',
+                'stripe_product_id' => 'prod_SQiYAwM7XMMPSD',
+                'stripe_price_id'   => 'price_1RVr7JK5z7ZAQPXQkc7kIQGt',
                 'interval'          => 'monthly',
-                'price'             => 199.99,
+                'price'             => 250.00,
+                'is_active'         => false,
             ],
         ];
 
         foreach ($plans as $plan) {
             SubscriptionPlan::create($plan);
         }
-    }
+     }
 }
