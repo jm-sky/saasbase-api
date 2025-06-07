170a0da4d18f89326bf8cefbd865c375ca47139a
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-06-07 10:36:52 +0200

    Update XmlSignatureVerificationResultDTO.php

--- app/Services/Signatures/DTOs/XmlSignatureVerificationResultDTO.php
+++ app/Services/Signatures/DTOs/XmlSignatureVerificationResultDTO.php
@@ -10,35 +10,3 @@ class XmlSignatureVerificationResultDTO
         public array $signatures,
     ) {}
 }
-
-class XmlSignatureDTO
-{
-    public function __construct(
-        public ?string $signingTime,
-        public CertificateDTO $certificate,
-        public SignerIdentityDTO $signerIdentity,
-    ) {}
-}
-
-class CertificateDTO
-{
-    public function __construct(
-        public string $issuer,
-        public string $serialNumber,
-        public ?string $validFrom = null,
-        public ?string $validTo = null,
-        public ?string $subject = null,
-    ) {}
-}
-
-class SignerIdentityDTO
-{
-    public function __construct(
-        public string $firstName,
-        public ?string $middleName,
-        public string $lastName,
-        public string $pesel,
-        public ?string $trustedProfileId = null,
-        public ?string $epuapUsername = null,
-    ) {}
-}
