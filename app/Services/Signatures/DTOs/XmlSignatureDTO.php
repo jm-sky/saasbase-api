9e91af0d330bef6010215506acefe649cf1061a7
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-06-07 10:54:46 +0200

    Update XmlSignatureDTO.php

--- app/Services/Signatures/DTOs/XmlSignatureDTO.php
+++ app/Services/Signatures/DTOs/XmlSignatureDTO.php
@@ -5,8 +5,15 @@ namespace App\Services\Signatures\DTOs;
 class XmlSignatureDTO
 {
     public function __construct(
-        public ?string $signingTime,
-        public CertificateDTO $certificate,
-        public SignerIdentityDTO $signerIdentity,
+        public bool $valid,
+        public bool $trustedCA,
+        public ?string $personFirstName,
+        public ?string $personLastName,
+        public ?string $personPESEL,
+        public ?string $certificateIssuer,
+        public ?string $certificateSerial,
+        public ?string $certificateSubject,
+        public ?string $certificateValidFrom,
+        public ?string $certificateValidTo,
     ) {}
 }
