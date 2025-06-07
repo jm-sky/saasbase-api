b7bbf98f404ca2d17eacfc64906ccc0bccc509de
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-06-07 11:19:19 +0200

    Create XmlSignatureVerifierService.php (#73)
    * Create XmlSignatureVerifierService.php
    
    * Create XmlSignatureVerificationResultDTO.php
    
    * Update XmlSignatureVerificationResultDTO.php
    
    * Create CertificateDTO.php
    
    * Create XmlSignatureDTO.php
    
    * Create SignerIdentityDTO.php
    
    * Create XmlSignatureVerifierService.php
    
    * Delete app/Services/XmlSignatureVerifierService.php
    
    * Update XmlSignatureDTO.php
    
    * Update XmlSignatureVerifierService.php

---
+++ app/Services/Signatures/DTOs/XmlSignatureVerificationResultDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Signatures\DTOs;
+
+class XmlSignatureVerificationResultDTO
+{
+    public function __construct(
+        public bool $valid,
+        /** @var XmlSignatureDTO[] */
+        public array $signatures,
+    ) {}
+}
