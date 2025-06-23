0cbdbca0a1a4b6aa42eea357be6a1d61d06bd269
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2025-06-23 20:38:05 +0200

    Create ExchangeRateDTO.php

---
+++ app/Services/NBP/DTOs/ExchangeRateDTO.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services\NBP\DTOs;
+
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+
+readonly class ExchangeRateDTO
+{
+    public function __construct(
+        public string $currencyCode,
+        public string $currencyName,
+        public float $rate,
+        public Carbon $effectiveDate,
+        public string $table,
+        public string $no
+    ) {}
+
+    public static function fromArray(array $data, string $effectiveDate, string $table, string $no): self
+    {
+        return new self(
+            currencyCode: $data['code'],
+            currencyName: $data['currency'],
+            rate: (float) $data['mid'],
+            effectiveDate: Carbon::parse($effectiveDate),
+            table: $table,
+            no: $no
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'currency_code' => $this->currencyCode,
+            'currency_name' => $this->currencyName,
+            'rate' => $this->rate,
+            'effective_date' => $this->effectiveDate->format('Y-m-d'),
+            'table' => $this->table,
+            'no' => $this->no
+        ];
+    }
+
+    public function toModel(): array
+    {
+        return [
+            'currency_code' => $this->currencyCode,
+            'currency_name' => $this->currencyName,
+            'rate' => $this->rate,
+            'effective_date' => $this->effectiveDate, // TODO: We want just "date" maybe 
+            'table' => $this->table,
+            'no' => $this->no
+        ];
+    }
+}
